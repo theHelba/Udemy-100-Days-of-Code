fa2faea5e9cb678ae02798e8fecad0afac9a58ca
chrisharley <chrisharley63@yahoo.com>
2025-03-18 21:57:01 -0500

    Day 5

---
+++ Day 005/For Loops wih Range.py
@@ -0,0 +1,15 @@
+# Range Function with For Loop
+
+# Prints 1-10
+for number in range(1, 11):
+    print(number)
+
+# Prints 1-10 in increments of 3
+for number in range(1, 11, 3):
+    print(number)
+
+# Gauss challenge
+total = 0
+for i in range(1, 101):
+    total += i
+print(total)
